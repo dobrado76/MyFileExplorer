87b6d70d6c5846e6ad1d334a72fc9cbc5c6a98fa
MeaningfulnessMediaGroup <info@meaningfulness.com.au>
2026-09-22 12:41:35 +1000

    Wrong import

--- scripts/patch-filerobot.mjs
+++ scripts/patch-filerobot.mjs
@@ -11,7 +11,6 @@
 import console from 'node:console'
 import fs from 'node:fs'
 import path from 'node:path'
-import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 import {
   patchFilerobotCropSource,
